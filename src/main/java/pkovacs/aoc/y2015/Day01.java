e8aba14960df3f7a09861f338d9ce95e371177fe
Péter Kovács <kovacspeter84@gmail.com>
2021-10-17 23:06:42 +0200

    Add solution for Day 1

--- src/main/java/pkovacs/aoc/y2015/Day01.java
+++ src/main/java/pkovacs/aoc/y2015/Day01.java
@@ -6,10 +6,19 @@ import pkovacs.util.InputUtils;
 public class Day01 {
 
     public static void main(String[] args) {
-        var lines = InputUtils.readLines(AocUtils.getInputPath());
+        var line = InputUtils.readSingleLine(AocUtils.getInputPath());
 
-        System.out.println("Part 1: " + 0);
-        System.out.println("Part 2: " + 0);
+        int floor = 0;
+        int firstBasement = -1;
+        for (int i = 0; i < line.length(); i++) {
+            floor += line.charAt(i) == '(' ? 1 : -1;
+            if (floor < 0 && firstBasement < 0) {
+                firstBasement = i + 1;
+            }
+        }
+
+        System.out.println("Part 1: " + floor);
+        System.out.println("Part 2: " + firstBasement);
     }
 
 }
